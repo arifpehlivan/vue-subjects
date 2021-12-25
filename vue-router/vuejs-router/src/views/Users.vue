64f3ff95010ffef522f76e4cb56a56b7c21b7146
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-12-25 17:18:57 +0300

    router

---
+++ vue-router/vuejs-router/src/views/Users.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      Users Page
+      <button @click="$router.push('/usersDetail/1')">Go to users detail page</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
