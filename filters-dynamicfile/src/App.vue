d85202486d153e6d2011e852c995a0245826e5cd
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-12-27 18:39:14 +0300

    create filter static file

--- filters-dynamicfile/src/App.vue
+++ filters-dynamicfile/src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
-    
+    <img :src="logos"/>
+    {{person}}
+    <p>{{name.toUpperCase()}}</p>
+    <p>{{name | upperCaseName}}</p>
   </div>
 </template>
 
 <script>
-
+import logo from './assets/logo.png'
 
 export default {
   name: 'App',
- 
+  data(){
+    return{
+      name: "Vue",
+      logos: logo,
+      person: require('../src/test.json')
+    }
+  },
+  filters:{
+    upperCaseName:function(value){
+      return value.toUpperCase()
+    }
+  }
 }
 </script>
 
