50a883f622c4c499e6add3993dcd5e661160b9db
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-12-30 00:54:46 +0300

    create refs

--- vue-refs/src/App.vue
+++ vue-refs/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    
+    <div>
+      <p ref="p1" id="p1">Hello World</p>
+      <p ref="p2" id="p2">Hello World 2</p>
+    </div>
+    <div>
+      <button @click="test">Click</button>
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,15 @@
 
 export default {
   name: 'App',
-  
+  methods:{
+    test(){
+      console.log(this.$refs)
+      console.log('idp1',document.getElementById('p1').innerHTML)
+      console.log('idp2',document.getElementById('p2').innerHTML)
+      console.log(this.$refs.p1.innerHTML)
+      console.log(this.$refs.p2.innerHTML)
+    }
+  }
 }
 </script>
 
